b9d8e0cf8d3aa47a6be70471bbcdf58fedafcfd3
Khubaib Husain <khubaibhusain45@gmail.com>
2025-12-28 20:32:38 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SignUp UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+var emailId = TextEditingController();
+var password = TextEditingController();
+var cPassword = TextEditingController();
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SignUp UI', style: TextStyle(color: Colors.purple)),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/m.jpg', fit: BoxFit.cover),
+          Container(color: Colors.black.withOpacity(0.4)),
+          Center(
+            child: SizedBox(
+              height: 400,
+              width: 450,
+              child: Card(
+                elevation: 6,
+                color: Colors.transparent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 30),
+                      child: Text(
+                        "SignUp Here",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 3,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          labelText: "Email ID",
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(Icons.email_rounded),
+                          suffixIconColor: Colors.white,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        controller: emailId,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 3,
+                            ),
+                          ),
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(Icons.password_outlined),
+                          suffixIconColor: Colors.white,
+                        ),
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        controller: password,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 3,
+                            ),
+                          ),
+                          labelText: "Confirm Password",
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(Icons.lock),
+                          suffixIconColor: Colors.white,
+                        ),
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        controller: cPassword,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          var mail = emailId.text.trim();
+                          var pass = password.text.trim();
+                          var cPass = cPassword.text.trim();
+
+                          if (mail.isEmpty || pass.isEmpty || cPass.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text("Error"),
+                                content: const Text(
+                                  "None of the fields can be empty",
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("OK"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else if (pass != cPass) {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text("Error"),
+                                content: const Text(
+                                  "Password and Confirm Password do not match",
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("OK"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text("Success"),
+                                content: const Text("Sign Up Successful"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("OK"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(color: Colors.purple, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
